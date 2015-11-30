a150a2b89f04ed6d378aae598a5ee6e6c6649353
Mihai Codoban <micodoba@microsoft.com>
2015-11-30 11:32:50 -0800

    Added initial bare bones ci shell script for Linux and OSX.

---
+++ cibuild.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+set -e
+
+usage()
+{
+    echo "Options"
+    echo "  --system-msbuild-path <path>   Path to the mono msbuild build used to build msbuild" 
+    echo "  --os <os>                      OS to run (Linux / Darwin)"
+    echo "  --scope <scope>                Scope of the build (Compile / Test)"
+}
+
+build()
+{
+	echo Build Command: "mono $MONO_ARGS"
+
+	mono $MONO_ARGS
+
+	echo Build completed. Exit code: $?
+	egrep "Warning\(s\)|Error\(s\)|Time Elapsed" "$LOG_PATH_ARG"
+	echo "Log: $LOG_PATH_ARG"
+}
+
+SCRIPT_PATH="`dirname \"$0\"`"
+
+#Default build arguments
+OS_ARG="OSX"
+TARGET_ARG="Build"
+LOG_PATH_ARG="$SCRIPT_PATH"/"msbuild.log"
+PROJECT_FILE_ARG="$SCRIPT_PATH"/"build.proj"
+
+while [[ $# > 0 ]]
+do
+    opt="$1"
+    case $opt in
+        -h|--help)
+        usage
+        exit 1
+        ;;
+        --system-msbuild-path)
+        SYSTEM_MSBUILD_PATH=$2
+        shift 2
+        ;;
+        --os)
+        OS_NAME=$2
+        shift 2
+        ;;
+        --scope)
+        SCOPE=$2
+        shift 2
+        ;;
+        *)
+        usage 
+        exit 1
+        ;;
+    esac
+done
+
+if [[ "$OS_NAME" = "Linux" ]]; then
+	OS_ARG="Unix"
+elif [[ "$OS_NAME" = "Darwin" ]]; then
+	OS_ARG="OSX"
+fi
+
+if [[ "$SCOPE" = "Compile" ]]; then
+	TARGET_ARG="Build"
+elif [[ "$SCOPE" = "Test" ]]; then
+	TARGET_ARG="BuildAndTest"
+fi
+
+MSBUILD_ARGS="$PROJECT_FILE_ARG /t:$TARGET_ARG /p:OS=$OS_ARG /p:Configuration=Debug-Netcore /verbosity:minimal"' "'"/fileloggerparameters:Verbosity=diag;LogFile=$LOG_PATH_ARG"'"'
+
+MONO_ARGS="$SYSTEM_MSBUILD_PATH $MSBUILD_ARGS"
+
+build
